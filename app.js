e5edeb15022f521fa294252114ed9e0b6e8c991c
Ayush04321 <patidarayush4321@gmail.com>
2026-09-19 14:38:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,6 +87,9 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 app.use("/", userRouter);
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
